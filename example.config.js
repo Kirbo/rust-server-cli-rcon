384493112f4b0d1ba5782909814512ae9ba293c8
Kimmo Saari <1077974+kirbo@users.noreply.github.com>
2019-03-18 04:41:16 +0200

    Update

--- example.config.js
+++ example.config.js
@@ -1,41 +1,41 @@
 var config = {
-    // name of this server block, used when using "server" argument
-    localhost: {
-        // configurations for server
-        server: {
-            // hostname or ip address
-            address: 'localhost'
-        },
-        // configurations for rcon
-        rcon: {
-            // rcon port
-            port: '28016',
-            // rcon password
-            password: 'your password'
-        }
+  // name of this server block, used when using "server" argument
+  localhost: {
+    // configurations for server
+    server: {
+      // hostname or ip address
+      address: 'localhost'
     },
+    // configurations for rcon
+    rcon: {
+      // rcon port
+      port: '28016',
+      // rcon password
+      password: 'rcon-password'
+    }
+  },
 
-    // another server block
-    another: {
-        server: {
-            address: 'another.rust-server.com'
-        },
-        rcon: {
-            port: '28016',
-            password: 'another password'
-        }
+  // another server block
+  another: {
+    server: {
+      address: 'another.rust-server.com'
     },
+    rcon: {
+      port: '28016',
+      password: 'another password'
+    }
+  },
 
-    // yet another server block
-    someOther: {
-        server: {
-            address: '1.2.3.4'
-        },
-        rcon: {
-            port: '28016',
-            password: 'yet another password'
-        }
+  // yet another server block
+  someOther: {
+    server: {
+      address: '1.2.3.4'
     },
+    rcon: {
+      port: '28016',
+      password: 'yet another password'
+    }
+  }
 };
 
 
